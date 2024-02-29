32ec31af4cfb280b0c3069f20a7c0dfac3ef98ed
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2024-02-29 16:32:23 +0900

    1.7.5b
    주석 정리

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// 내가 입력한 데이터를 localStorage에 세이브
+// 앱 로드 시 실행되는 기능들 (이하 初)
+// 初 내가 입력한 데이터를 localStorage에 세이브
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks)); // 할일 목록을 tasks라는 이름으로 로컬 저장소에 저장
 }
 
-// localStorage에 저장된 데이터를 로드
+// 初 localStorage에 저장된 데이터를 로드
 function loadTasks() {
     const storedTasks = localStorage.getItem("tasks"); // 로컬 저장소에 저장된 tasks를 가져옴
     if (storedTasks) {
@@ -13,10 +14,10 @@ function loadTasks() {
     }
 }
 
-// 초기 할일 목록 로드
+// 初 초기 할일 목록 로드
 const tasks = loadTasks();
 
-// Notification 허가 요청
+// 初 Notification 허가 요청
 if (Notification.permission !== "granted" && Notification.permission !== "denied") {
     Notification.requestPermission().then((permission) => {
         if (permission === "granted") {
@@ -27,7 +28,12 @@ if (Notification.permission !== "granted" && Notification.permission !== "denied
     });
 }
 
-// 알림 기능
+// 핵심 기능들 (이하 ☆)
+// 핵심 기능이 손상되면 해당 앱은 제대로 사용할 수 없음
+// ☆ 시간 동기화 기능
+setInterval(checkNotifications, 1000);
+
+// ☆ 알림 기능
 function checkNotifications() {
     const now = new Date(); // 현재 시간 가져오기
 
@@ -48,7 +54,7 @@ function checkNotifications() {
     });
 }
 
-// 알림 표시 함수
+// ☆ 알림 표시 함수
 function showNotification(message) {
     if (Notification.permission === "granted") {
         new Notification("リマインド", { body: message });
@@ -63,9 +69,7 @@ function showNotification(message) {
     }
 }
 
-// 디바이스와 시간 동기화를 위해 setInterval 함수로 1초마다 checkNotifications 함수를 실행
-setInterval(checkNotifications, 1000);
-
+// ☆ 알림 목록에 할일이 있는지 확인하는 함수
 function isTaskInRemindList(task) {
     const remindList = document.getElementById("remindList");
     const remindItems = remindList.getElementsByClassName("remind");
@@ -77,7 +81,7 @@ function isTaskInRemindList(task) {
     return false;
 }
 
-// 할일 목록 렌더링
+// ☆ 할일 목록 렌더링
 function renderTasks() {
     const taskList = document.getElementById("taskList");
     const remindList = document.getElementById("remindList");
@@ -145,28 +149,7 @@ function renderTasks() {
     saveTasks();
 }
 
-// 날짜 및 시간 형식 지정 함수
-function formatDate(date) {
-    const now = new Date();
-    const diff = date.getDate() - now.getDate();
-
-    if (diff === -2) {
-        return "一昨日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
-    } else if (diff === -1) {
-        return "昨日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
-    } else if (diff === 0) {
-        return "今日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
-    } else if (diff === 1) {
-        return "明日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
-    } else if (diff === 2) {
-        return "明後日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
-    } else {
-        const options = { year: "numeric", month: "numeric", day: "numeric", hour: "2-digit", minute: "2-digit" };
-        return date.toLocaleDateString("ja-JP", options).replace(/\//g, '/') + " " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
-    }
-}
-
-// 할일 추가
+// ☆ 할일 추가
 function addTask() {
     const taskInput = document.getElementById("taskInput"); // 할일 내용 입력란
     const datetimeInput = document.getElementById("datetimeInput"); // 할일 시간 입력란
@@ -192,30 +175,7 @@ function addTask() {
     }
 }
 
-// 알람 반복 기능 토글
-function toggleRepeatAlarm() {
-    const repeatAlarm = document.getElementById("repeatAlarm").checked;
-    localStorage.setItem("repeatAlarm", repeatAlarm);
-}
-
-// Re-Remind 기능
-// 알람 반복 기능이 켜져있다면, 10분마다 반복해서 알림 표시하도록 설정
-window.onload = function () {
-    const repeatAlarm = localStorage.getItem("repeatAlarm");
-    if (repeatAlarm === "true") {
-        setInterval(checkNotifications, 600000);
-    }
-};
-
-// 알람 반복 기능 설정 저장값 불러오기
-window.onload = function () {
-    const repeatAlarm = localStorage.getItem("repeatAlarm");
-    if (repeatAlarm !== null) {
-        document.getElementById("repeatAlarm").checked = JSON.parse(repeatAlarm);
-    }
-};
-
-// 할일 목록 완료 토글
+// ☆ 할일 목록 완료 토글
 function toggleCompleted(id) {
     const task = tasks.find((task) => task.id === id);
     if (task) {
@@ -224,23 +184,69 @@ function toggleCompleted(id) {
     }
 }
 
-// 목록 접기/펼치기 기능 구현 함수들 (이하 접펼)
-// 접펼 リマインド
+// 일반 기능들 (이하 N)
+// N 날짜 및 시간 형식 지정 함수
+function formatDate(date) {
+    const now = new Date();
+    const diff = date.getDate() - now.getDate();
+
+    if (diff === -2) {
+        return "一昨日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
+    } else if (diff === -1) {
+        return "昨日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
+    } else if (diff === 0) {
+        return "今日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
+    } else if (diff === 1) {
+        return "明日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
+    } else if (diff === 2) {
+        return "明後日 " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
+    } else {
+        const options = { year: "numeric", month: "numeric", day: "numeric", hour: "2-digit", minute: "2-digit" };
+        return date.toLocaleDateString("ja-JP", options).replace(/\//g, '/') + " " + date.toLocaleTimeString("ja-JP", { hour: '2-digit', minute: '2-digit' }).replace(":", "時 ") + "分";
+    }
+}
+
+// Re-Remind 기능 (이하 R)
+// R 알람 반복 기능 토글
+function toggleRepeatAlarm() {
+    const repeatAlarm = document.getElementById("repeatAlarm").checked;
+    localStorage.setItem("repeatAlarm", repeatAlarm);
+}
+
+// R 알람 반복 기능이 켜져있다면, 10분마다 반복해서 알림 표시하도록 설정
+window.onload = function () {
+    const repeatAlarm = localStorage.getItem("repeatAlarm");
+    if (repeatAlarm === "true") {
+        setInterval(checkNotifications, 600000);
+    }
+};
+
+// R 알람 반복 기능 설정 저장값 불러오기
+window.onload = function () {
+    const repeatAlarm = localStorage.getItem("repeatAlarm");
+    if (repeatAlarm !== null) {
+        document.getElementById("repeatAlarm").checked = JSON.parse(repeatAlarm);
+    }
+};
+
+// 목록 접기/펼치기 기능 구현 함수들 (이하 F)
+// F リマインド
 function toggleRemindList() {
     $('#remindList').slideToggle();
 }
 
-// 접펼 予定
+// F 予定
 function toggleTaskList() {
     $('#taskList').slideToggle();
 }
 
-// 접펼 完了済み
+// F 完了済み
 function toggleCompletedList() {
     $('#completedList').slideToggle();
 }
 
-// 할일 목록 완료
+// 버튼 기능 구현 함수들 (이하 B)
+// B 할일 목록 완료
 function completeTask(id) {
     const task = tasks.find((task) => task.id === id);
     if (task) {
@@ -249,7 +255,7 @@ function completeTask(id) {
     }
 }
 
-// 할일 목록 스누즈
+// B 할일 목록 스누즈
 function snoozeTask(id) {
     const task = tasks.find((task) => task.id === id);
     if (task) {
@@ -261,7 +267,7 @@ function snoozeTask(id) {
     }
 }
 
-// 할일 목록 삭제
+// B 할일 목록 삭제
 function deleteTask(id) {
     const index = tasks.findIndex((task) => task.id === id);
     if (index !== -1) {
@@ -270,11 +276,14 @@ function deleteTask(id) {
     }
 }
 
+// 앱 초기화 및 종료 (이하 E)
+// E 앱 초기화
 function resetApp() {
     localStorage.clear();
     location.reload();
 }
 
+// E 앱 종료
 function exitApp() {
     window.close();
 }
