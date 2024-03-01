5b0c492b7a5850c2a3ca9b73e32e2548e363ad95
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2024-03-01 18:07:06 +0900

    1.8.1

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const { app, BrowserWindow, screen } = require('electron');
+const { app, BrowserWindow, screen, Tray, Menu } = require('electron');
 const path = require('path');
+
+let mainWindow;
+let tray;
 
 // 윈도우 크기 계산
 function calculateWindowSize() {
@@ -15,21 +18,28 @@ function calculateWindowSize() {
 // 윈도우 생성 시 기본 옵션
 function createWindow() {
     const { width, height } = calculateWindowSize();
-    const win = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: width,
         height: height,
         resizable: false,
         frame: false,
         autoHideMenuBar: true,
+        icon: path.join(__dirname, 'icon.png'),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     });
 
-    win.loadFile('index.html');
+    mainWindow.loadFile('index.html');
+
+    // 창을 닫을 때 트레이로 최소화
+    mainWindow.on('close', (event) => {
+        event.preventDefault();
+        mainWindow.hide();
+    });
 }
 
-// 윈도우 생성 명령
+// 앱이 준비되면 윈도우를 생성
 app.whenReady().then(() => {
     createWindow();
 
@@ -38,9 +48,19 @@ app.whenReady().then(() => {
             createWindow();
         }
     });
+
+    // 트레이 생성
+    tray = new Tray(path.join(__dirname, 'icon.png'));
+    const contextMenu = Menu.buildFromTemplate([
+        { label: '開く', click: () => mainWindow.show() },
+        { label: '隠す', role: 'quit' },
+        { label: '終了', click: () => mainWindow.close()}
+    ]);
+    tray.setToolTip('リマインド');
+    tray.setContextMenu(contextMenu);
 });
 
-// 앱 종료
+// 모든 창이 닫히면 애플리케이션 종료
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
