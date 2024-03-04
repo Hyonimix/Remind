6e824bea18afe67118f8f32d312b5e9f66722b56
D5 Kan <86899162+Hyonimix@users.noreply.github.com>
2024-03-04 14:34:36 +0900

    1.8.3c

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
 const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 
-// 윈도우 크기 계산
-function calculateWindowSize() {
-    const aspectRatio = 9 / 21;
-    const targetWidth = 320; // 고정 픽셀 가로 크기
-    const targetHeight = targetWidth / aspectRatio; // 세로 크기 계산
-
-    return { width: targetWidth, height: targetHeight };
-}
-
 // 윈도우 생성 시 기본 옵션
 function createWindow() {
-    const { width, height } = calculateWindowSize();
     const win = new BrowserWindow({
-        width: width,
-        height: height,
+        width: 360,
+        height: 640,
         resizable: false,
         frame: false,
         autoHideMenuBar: true,
         icon: path.join(__dirname, 'icon.png'),
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
+            preload: path.join(__dirname, 'preload.js'),
+            // offscreen: true
+        },
+        // movable: true
     });
 
     win.loadFile('index.html');
